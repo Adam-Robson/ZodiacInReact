3d47c532515bcf1e8a41414f7bcd40c4525857b9
Adam Robson <adamray312@gmail.com>
2022-09-20 16:57:46 -0700

    configure folder-file tree readme wireframe

--- src/App.js
+++ src/App.js
@@ -23,3 +23,14 @@ function App() {
 }
 
 export default App;
+
+// Rubric
+//     +0.5    Main branch deployed to Netlify X
+//     +0.5    Open PR from dev branch with Netlify deploy preview
+//     +3      Header and Footer components
+//     +4      Main component which imports list of Zodiac signs
+//     +4      Uses a .map to pass props to a child component
+//     +2      Child component displays sign’s image, name and dates
+//     +2      Styled using individual style sheets imported into components
+//     +2      At least one component uses a background image using an import
+//     +2      Zodiac images are loaded dynamically from the public directory
