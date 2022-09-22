a4d6c9209444809c66bf231f6bf0dc2006c75b8c
Adam Robson <adamray312@gmail.com>
2022-09-22 16:35:50 -0700

    'add classes to elements'

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -1,12 +1,13 @@
 import './Zodiac.css';
 
-export default function Zodiac({ title, image, dates }) {
+export default function Zodiac({ id, title, image, dates }) {
   return (
     <div className='card'>
       <img src={process.env.PUBLIC_URL + `/img/${title}.png`} />
-      <p className='name'>{title}</p>
-      <p>{image}</p>
-      <p>{dates}</p>
+      <p className="title">{title}</p>
+      <img src={ `${image}`.png } alt={ title } />
+      <p className="dates">{ dates }</p>
+      <p className="id">{ id }</p>
     </div>
   );
 }
