5ce725d55b12932556a036707e3789ffd2167b47
Adam Robson <adamray312@gmail.com>
2024-01-30 14:18:03 -0800

    create a component for the toggle button in the detail page for zodiac

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Zodiac from './pages/Zodiac'
+import ZodiacDetail from './pages/ZodiacDetail'
+import Layout from './components/Layout'
 
 export default function App() {
   return (
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/zodiac" element={<Zodiac />} />
-      <Route path="/zodiac/:sign" element={<Zodiac />} />
-      </Routes>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/zodiac" element={<Zodiac />} />
+        <Route path="/zodiac/:sign" element={<ZodiacDetail />} />
+      </Route>
+    </Routes>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,9 @@
 import { Link } from "react-router-dom"
-import BackgroundContainer from "../components/BackgroundContainer"
-import steal from '../assets/steal.svg'
 
 export default function Home() {
   return (
-    <BackgroundContainer backgroundImage={steal}>
-    <main className="h-full w-full flex flex-col justify-around items-center">
+    <main className="min-h-screen w-full flex flex-col justify-center items-center">
       <Link to="/zodiac" className="text-5xl/9 font-normal subpixel-antialiased">zōdēˌak</Link>
-      </main>
-    </BackgroundContainer>
+    </main>
   )
 }
